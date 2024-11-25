1570cdd9aa0ddea5d278a486366ff24a0355b798
Olumuyiwa Folorunso <olumuyiwa.folorunso@gmail.com>
2024-11-25 17:57:17 +0000

    chore(UNIP-001):HelloWorldController changes and pom changes

--- src/main/java/com/poc/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/poc/helloworld/controller/HelloWorldController.java
@@ -7,22 +7,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
 
 @RestController
 @RequestMapping("/helloworld")
 public class HelloWorldController {
+    private static final Logger logger = 	LoggerFactory.getLogger(HelloWorldController.class);
+
 
     @Autowired
     private HelloWorldService helloWorldService;
     //    /helloworld/greet
     @GetMapping(path = "/greet")
     public ResponseEntity<String> greetAsJson() {
+        logger.info("greetAsJson request received at '{}'", new Date().toString());
+
         JSONObject jsonObject = helloWorldService.fetchRightGreeting();
+
         return ResponseEntity.ok(jsonObject.toString());
     }
     //    /helloworld/sayhi
     @GetMapping(path = "/sayhi")
     public String greetAsString() {
+        logger.info("greetAsString request received at '{}'.", new Date());
         return helloWorldService.getTimeOfDay();
     }
 }
